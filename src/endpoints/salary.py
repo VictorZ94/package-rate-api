11e00cabb49e2b864efd4121556c491daf27b4bc
Victor Zuluaga <zrvictor@outlook.com>
2023-05-11 19:03:34 -0500

    setting up query validations to route salaries

--- src/endpoints/salary.py
+++ src/endpoints/salary.py
@@ -2,7 +2,7 @@ import json
 import datetime
 from typing import Optional
 from fastapi.responses import JSONResponse
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 router = APIRouter(tags=["Salaries"])
 
@@ -10,7 +10,7 @@ with open('fake_data/salario.json') as salary_file:
   data_smmlv = json.load(salary_file)
 
 @router.get('/salary-per-year')
-def salary_per_year(year: Optional[int] = datetime.date.today().year):
+def salary_per_year(year: Optional[int] = Query(default=datetime.date.today().year, ge=1990, le=datetime.date.today().year)):
   """ Method to fetch smmlv per year in Colombian.
   """
   smmlv_value = data_smmlv.copy()
