acdceef09b257ff90c33a3f2b2047c075880c23f
Victor Zuluaga <zrvictor@outlook.com>
2023-05-15 18:46:16 -0500

    added new route for fetching uvt values Apply to Colombia

--- routes/api.py
+++ routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from src.endpoints import salary, aux_trans
+from src.endpoints import salary, aux_trans, uvt
 
 router = APIRouter()
 router.include_router(salary.router)
 router.include_router(aux_trans.router)
+router.include_router(uvt.router)
